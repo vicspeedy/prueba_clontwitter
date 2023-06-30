e0289e8d0312d858a87da10eff228ec3f4f6b841
vicspeedy <botesh2k@gmail.com>
2023-06-30 21:22:52 +0000

    commit routes:root, datos de prueba seeds y migracion:seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+30.times do |i|
+    # revisar @tweets con minuscula en plural y el Tweet.create Modelo con mayuscula y en sigular 
+    @tweets = Tweet.create(username: "Username #{i}", description: "Tweets #{i} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium")
+
+    print @tweets
+end
